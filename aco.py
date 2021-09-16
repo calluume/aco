7f19d92c068c7f32771eacf39533d48315a70588
stebey <cdevans98@gmail.com>
2021-09-16 16:05:03 +0100

    Added parameters & bug fixes
    - Changed parameters to dictionary, so order does not matter when creating aco object.
    - Added deposit and evaporation rate controls. Food depleting as ants collect food can also be disabled.
    - Fixed end/pause button, so the simulation now ends/pauses when clicked.
    - Added documentation.
    - Added the ability to give ants random colours to make it easier to follow individual ants.

--- aco.py
+++ aco.py
@@ -5,7 +5,7 @@ import time
 movements = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
 
 class aco:
-    def __init__(self, dimensions, no_ants, no_food, carry_capacity=3, food_capacity=10, home_coors=[0,0], alpha=10, beta=1, random_ants=False, verbose=False):
+    def __init__(self, dimensions, no_ants, no_food, carry_capacity=3, food_capacity=10, home_coors=[0,0], pheromone_deposit=1000.0, evaporation_coefficient=0.02, alpha=10, beta=1, food_deplete=True, random_ant_colour=False, verbose=False):
         self.ants = []
         self.food = {}
         self.dimensions = dimensions
@@ -13,10 +13,12 @@ class aco:
         self.home_coors = home_coors
         self.all_food = no_food*food_capacity
         self.brought_food = 0
-        self.evapouration_coefficient = 0.02
+        self.evaporation_coefficient = 0.02
         self.pheromone_deposit = 1000.0
         self.alpha = alpha
         self.beta = beta
+        self.food_deplete = food_deplete
+        self.random_ant_colour = random_ant_colour
 
         if no_ants + no_food > dimensions[0]*dimensions[1]:
             print("not enough cells")
@@ -29,24 +31,21 @@ class aco:
                 self.food[str(coors[0])+","+str(coors[1])] = Food(coors, food_capacity)
                 food_coors.append(coors)
 
-        if random_ants:
-            ant_coors = []
-            while len(ant_coors) < no_ants:
-                coors = [rnd.randint(0, dimensions[0]-1), rnd.randint(0, dimensions[1]-1)]
-                if coors not in ant_coors and coors not in food_coors:
-                    self.ants.append(Ant(coors, carry_capacity=carry_capacity))
-                    ant_coors.append(coors)
-
-        else:
-            ants_created = 0
-            while ants_created < no_ants:
-                coors = self.home_coors
-                self.ants.append(Ant(coors))
-                ants_created += 1
+        ant_coors = []
+        
+        ants_created = 0
+        while ants_created < no_ants:
+            coors = self.home_coors
+            self.ants.append(Ant(coors, carry_capacity=carry_capacity, random_colour=self.random_ant_colour))
+            ants_created += 1
 
         self.ant_grid, self.food_grid = self.get_object_grids()
 
     def start_aco(self):
+        """
+        Starts the ant colony optimisation, printing the grid
+        to the console.
+        """
         self.print_grid()
         while(True):
 
@@ -56,9 +55,13 @@ class aco:
             self.print_grid()
             time.sleep(0.5)
 
-        print("done")
+        print("Simulation ended.")
 
     def increment(self):
+        """
+        Increments the simulation.
+        :return int: Total food left
+        """
         self.move_ants()
         self.ant_grid, self.food_grid = self.get_object_grids()
         total_food = 0
@@ -69,6 +72,10 @@ class aco:
         return total_food
         
     def is_finished(self):
+        """
+        Checks if all food is collected.
+        :return bool: If all food is collected
+        """
         carried_food = 0
         for ant in self.ants:
             carried_food += ant.current_carry
@@ -76,6 +83,9 @@ class aco:
         return (self.all_food == self.brought_food) or (carried_food == 0 and len(self.food.keys()) == 0)
 
     def move_ants(self):
+        """
+        Moves all ants according to movement probabilities.
+        """
         for ant in self.ants:
             for key in ant.taboo.keys():
                 if ant.taboo[key] >= 1:
@@ -91,7 +101,8 @@ class aco:
                     ant.taboo[str(x)+","+str(y)] = ant.taboo_cooldown
                     ant.current_carry += 1
                     if self.food[taboo_key].food_val > 1:
-                        self.food[taboo_key].food_val -= 1
+                        if self.food_deplete:
+                            self.food[taboo_key].food_val -= 1
                     else: 
                         #self.food[taboo_key].food_val = 0
                         _ = self.food.pop(taboo_key)
@@ -99,13 +110,16 @@ class aco:
                     self.brought_food += ant.current_carry
                     ant.current_carry = 0
                 
-                if ant.current_carry == ant.carry_capacity:
-                    self.pheromones[x][y] += self.pheromone_deposit
             self.ant_grid, self.food_grid = self.get_object_grids()
             self.update_pheromones()
 
     
     def get_movement_probs(self, ant):
+        """
+        Finds an ant's movement probabilities for the 8 adjacent tiles.
+        :param ant:      Ant to move
+        :return [float]: Movement probabilties
+        """
         centre = ant.location
         mov_weights = []
         food_present = False
@@ -162,6 +176,7 @@ class aco:
                 elif mov_weights[i] > 0: mov_weights[i] = 1
 
         """
+        # Implementing proper movement probability formula
         if sum(mov_weights) == 0:
             return mov_weights
         else:
@@ -173,16 +188,36 @@ class aco:
         return mov_weights
 
     def update_pheromones(self):
+        """
+        Applies pheromone evaporation and deposits.
+        """
         for i in range(self.pheromones.shape[0]):
             for j in range(self.pheromones.shape[1]):
-                self.pheromones[i][j] *= (1 - self.evapouration_coefficient)
+                self.pheromones[i][j] *= (1 - self.evaporation_coefficient)
                 if self.pheromones[i][j] < 1.0:
                     self.pheromones[i][j] = 1.0
 
+        for ant in self.ants:
+            x, y = ant.location[0], ant.location[1]
+            if ant.current_carry == ant.carry_capacity:
+                self.pheromones[x][y] += self.pheromone_deposit
+
     def manhattan(self, coor):
+        """
+        Calculates manhattan distance from coordinate to home.
+        :param coor: Starting coordinate
+        :return int: Manhattan distance to home
+        """
         return abs(coor[0]-self.home_coors[0]) + abs(coor[1]-self.home_coors[1])
 
     def print_grid(self, home_char='H', ant_char='A', food_char='F', empty_char='.'):
+        """
+        Prints grid to the console.
+        :param home_char:  Character at home coor
+        :param ant_char:   Character for all ants
+        :param food_char:  Character for food
+        :param empty_char: Character for empty tile
+        """
         ant_grid, food_grid = self.get_object_grids()
         print(" "+("-"*(ant_grid.shape[1]*2+1)))
         for i in range(ant_grid.shape[0]):
@@ -197,6 +232,11 @@ class aco:
         print(" "+("-"*(ant_grid.shape[1]*2+1)))
 
     def get_object_grids(self):
+        """
+        Returns grids denoting where ants and food are located.
+        :return np.array: Numpy array denoting ants
+        :return np.array: Numpy array denoting food
+        """
         ant_grid = np.zeros(self.dimensions, dtype=int)
         food_grid = np.zeros(self.dimensions, dtype=int)
         ant_coors, food_coors = self.get_object_coors()
@@ -207,6 +247,11 @@ class aco:
         return ant_grid, food_grid
 
     def get_object_coors(self):
+        """
+        Returns coordinates of all ants and food.
+        :return [[int]]: All ant coordinates
+        :return [[int]]: All food coordinates
+        """
         ant_coors = []
         for ant in self.ants:
             ant_coors.append(ant.location)
@@ -217,12 +262,16 @@ class aco:
         return ant_coors, food_coors
 
 class Ant:
-    def __init__(self, location, carry_capacity=3, taboo_cooldown=5):
+    def __init__(self, location, carry_capacity=3, taboo_cooldown=5, random_colour=False):
         self.location = location
         self.taboo = {}
         self.taboo_cooldown = taboo_cooldown
         self.carry_capacity = carry_capacity
         self.current_carry = 0
+        if random_colour:
+            self.colour = ("#%06x" % rnd.randint(0x55555, 0xFFFFFF)).upper()
+        else:
+            self.colour = "#FF0000"
 
 class Food:
     def __init__(self, location, food_capacity=10):
@@ -231,5 +280,5 @@ class Food:
         self.food_capacity = food_capacity
 
 if __name__ == "__main__":
-    acoApp = aco((20, 20), 25, 10, home_coors=[10, 10])
+    acoApp = aco((20, 20), 25, 3, home_coors=[10, 10])
     acoApp.start_aco()
--- app.py
+++ app.py
@@ -7,12 +7,19 @@ class paramWindow:
     def __init__(self):
         self.param_window = tk.Tk()
         self.inputs = {}
-        self.parameters = []
+        self.parameters = {}
         self.valid_params = False
 
         self.param_window.title('ACO')
 
     def add_parameter(self, paramId, dataType, default, valRange=None, label="Parameter"):
+        """
+        Adds parameter to the input dictionary.
+        :param paramId:  Unique id for dictionary
+        :param dataType: Parameter data type (ints, floats, booleans supported)
+        :param valRange: Value range for ints/floats
+        :param label:    Label displayed to user
+        """
         self.inputs[paramId] = {
             'label': label,
             'entry': None,
@@ -23,6 +30,9 @@ class paramWindow:
         }
         
     def display_window(self):
+        """
+        Displays the parameter window, with all defined inputs to the user.
+        """
         frame = tk.Frame(master=self.param_window)
         frame.grid(padx=10, pady=20, columnspan=2)
         tk.Label(master=frame, text="Enter simulation parameters").pack()
@@ -35,12 +45,12 @@ class paramWindow:
             input_dict = self.inputs[input_key]
             
             frame = tk.Frame(master=self.param_window)
-            frame.grid(row=self.rows, column=0, padx=10, pady=5)
+            frame.grid(row=self.rows, column=0, padx=10, pady=1)
             input_dict['label'] = tk.Label(master=frame, text=input_dict['label'])
             input_dict['label'].pack()
 
             frame = tk.Frame(master=self.param_window)
-            frame.grid(row=self.rows, column=1, padx=10, pady=5)
+            frame.grid(row=self.rows, column=1, padx=10, pady=1)
             input_dict['entry'] = tk.Entry(master=frame, width=10)
             input_dict['entry'].insert(0, input_dict['default'])
             input_dict['entry'].pack()
@@ -57,13 +67,20 @@ class paramWindow:
         return self.parameters
 
     def update_values(self):
+        """
+        Updates entered values in input dictionary.
+        """
         for key in self.inputs.keys():
             value = self.inputs[key]['entry'].get()
             self.inputs[key]['value'] = value
 
     def submit_values(self, event):
+        """
+        Submits values, testing for validity. Destroys the window
+        and sets valid_params to true if all inputs are valid.
+        """
         self.update_values()
-        self.parameters = []
+        self.parameters = {}
         
         failed = False
         for key in self.inputs.keys():
@@ -89,7 +106,7 @@ class paramWindow:
                         self.status_text.set("Values out of range: "+str(valRange[0])+" - "+str(valRange[1]))
                         failed = True
                         
-                self.parameters.append(value)
+                self.parameters[key] = value
                 
             except ValueError:
                 input_dict['label'].config(fg='red')
@@ -102,32 +119,54 @@ class paramWindow:
             self.status = tk.Label(master=frame, textvariable=self.status_text)
             self.status.pack()
 
+        if self.parameters['ants'] + self.parameters['food'] > self.parameters['x']*self.parameters['y']:
+            self.status.config(fg='red')
+            self.status_text.set("Not enough cells for amount of food & ants.") 
+            failed = True
+
         if not failed:
             self.status.config(fg='green')
             self.status_text.set("Done!")
             self.param_window.after(1000, self.param_window.destroy)
+            self.valid_params = True
         else:
             self.status.config(fg='red')
-            self.parameters = []
+            self.parameters = {}
 
 class mainWindow:
     def __init__(self, parameters):
-        self.columns = parameters[0]
-        self.rows = parameters[1]
-        self.ants = parameters[2]
-        self.food = parameters[4]
-        self.speed = parameters[6]
+        self.columns = parameters['x']
+        self.rows = parameters['y']
+        self.ants = parameters['ants']
+        self.food = parameters['food']
+        self.speed = parameters['speed']
 
-        self.aco = aco((self.rows, self.columns), self.ants, self.food, carry_capacity=parameters[3], food_capacity=parameters[5], home_coors=[int(self.rows/2), int(self.columns/2)])
+        self.aco = aco((self.rows, self.columns),
+                       self.ants, self.food,
+                       carry_capacity=parameters['carry'],
+                       food_capacity=parameters['fsize'],
+                       pheromone_deposit=parameters['deposit'],
+                       evaporation_coefficient=parameters['evap'],
+                       home_coors=[int(self.rows/2), int(self.columns/2)],
+                       food_deplete=parameters['deplete'],
+                       random_ant_colour=parameters['antcolour'])
         
         self.grid = []
         
         self.running = False
 
     def display(self):
+        """
+        Displays the main simulation window.
+        """
         self.main_window = tk.Tk()
         self.status_text = tk.StringVar()
         self.status_text.set("Click start to run the simulation.")
+
+        self.start_btn_text = tk.StringVar()
+        self.start_btn_text.set("Start")
+        self.pause_btn_text = tk.StringVar()
+        self.pause_btn_text.set("End Simulation")
 
         self.grid_frame = tk.Frame(master=self.main_window, relief=tk.RAISED,  borderwidth=1)
         self.grid_frame.grid(padx=10, pady=10)
@@ -147,15 +186,15 @@ class mainWindow:
 
         frame = tk.Frame(master=self.main_window)
         frame.grid(padx=10, pady=5, columnspan = self.columns)
-        self.submit_btn = tk.Button(master=frame, text="Start", width=10)
+        self.submit_btn = tk.Button(master=frame, textvariable=self.start_btn_text, width=15)
         self.submit_btn.pack()
         self.submit_btn.bind("<Button-1>", self.start_aco)
 
         frame = tk.Frame(master=self.main_window)
         frame.grid(padx=10, pady=5, columnspan = self.columns)
-        self.submit_btn = tk.Button(master=frame, text="End", width=10)
-        self.submit_btn.pack()
-        self.submit_btn.bind("<Button-1>", self.start_aco)
+        self.pause_btn = tk.Button(master=frame, textvariable=self.pause_btn_text, width=15)
+        self.pause_btn.pack()
+        self.pause_btn.bind("<Button-1>", self.end_aco)
 
         frame = tk.Frame(master=self.main_window, width=10, height=15)
         frame.grid(columnspan = self.columns)
@@ -164,17 +203,29 @@ class mainWindow:
 
     def start_aco(self, event):
         """
-        for ant in self.aco.ants:
-            ant_coors = ant.location
-            self.grid[ant_coors[0]][ant_coors[1]].config(bg='red')
-            """
-        self.running = True
-        self.main_window.after(100, self.update_aco)
+        Starts the ant colony optimisation simulation.
+        """
+        if not self.running:
+            self.start_btn_text.set("Start")
+            self.pause_btn_text.set("Pause")
+            self.running = True
+            self.main_window.after(int(self.speed), self.update_aco)
 
     def end_aco(self, event):
-        self.running = False
+        """
+        Ends the simulation.
+        """
+        if self.running:
+            self.start_btn_text.set("Resume")
+            self.pause_btn_text.set("Exit Simulation")
+            self.running = False
+        else: 
+            self.main_window.destroy()
 
     def update_aco(self):
+        """
+        Updates the ant colony optimisation grid.
+        """
         if self.running:
             for x in range(self.columns):
                 for y in range(self.rows):
@@ -182,7 +233,7 @@ class mainWindow:
                     self.grid[x][y].config(bg=colour)
             for ant in self.aco.ants:
                 ant_coors = ant.location
-                self.grid[ant_coors[0]][ant_coors[1]].config(bg='red')
+                self.grid[ant_coors[0]][ant_coors[1]].config(bg=ant.colour)
 
             home_coors = self.aco.home_coors
             self.grid[home_coors[0]][home_coors[1]].config(bg='blue')
@@ -194,8 +245,10 @@ class mainWindow:
             total_food = self.aco.increment()
             brought_food = self.aco.brought_food
             if not self.aco.is_finished():
-                self.status_text.set("Total: "+str(total_food)+", brought: "+str(brought_food))
-                self.main_window.after(100, self.update_aco)
+                if self.aco.food_deplete:
+                    self.status_text.set("Food left: "+str(total_food)+", food brought home: "+str(brought_food))
+                else: self.status_text.set("Food brought home: "+str(brought_food))
+                self.main_window.after(int(self.speed), self.update_aco)
             else:
                 self.status.config(fg='green')
                 self.status_text.set("Simulation complete!")
@@ -226,10 +279,15 @@ if __name__ == "__main__":
     param_input.add_parameter('ants', dataType=int, default=25, label='Number of ants:',  valRange=[0, 100])
     param_input.add_parameter('carry', dataType=int, default=3, label='Ant carry capacity:',  valRange=[1, 10])
     param_input.add_parameter('food', dataType=int, default=10, label='Amount of food:',  valRange=[1, 20])
-    param_input.add_parameter('fsize', dataType=int, default=10, label='Size of food',  valRange=[1, 20])
+    param_input.add_parameter('fsize', dataType=int, default=10, label='Size of food:',  valRange=[1, 20])
+    param_input.add_parameter('deplete', dataType=bool, default='True', label='Food depletion:')
+    param_input.add_parameter('deposit', dataType=int, default=1000, label='Pheromone deposit:',  valRange=[1, 100000])
+    param_input.add_parameter('evap', dataType=int, default=5, label='Evaporation Rate (%):',  valRange=[0, 100])
     param_input.add_parameter('speed', dataType=float, default=100, label='Simulation Speed (%):',  valRange=[10, 500])
+    param_input.add_parameter('antcolour', dataType=bool, default='False', label='Random ant colour:')
 
     parameters = param_input.display_window()
-    
-    main = mainWindow(parameters)
-    main.display()
+
+    if param_input.valid_params:
+        main = mainWindow(parameters)
+        main.display()
